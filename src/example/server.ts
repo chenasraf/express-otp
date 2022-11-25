44b01745c2b21be8a456c8dc6dd611628c319ab5
Chen Asraf <chenasrafil@gmail.com>
2022-11-25 11:19:56 +0200

    feat: generic handlers

--- src/example/server.ts
+++ src/example/server.ts
@@ -1,66 +1,31 @@
 import express from 'express'
-import totp from 'totp-generator'
-import { base32secret, generateQR, generateTokenUri } from '..'
+import otipi from '..'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
-const sampleUser = { id: 1, username: '', secret: '' } // base32secret()
+const sampleUser = { id: 1, username: process.env.USER_NAME!, secret: process.env.USER_SECRET! }
 
-app.use('/generate', (req, res) => {
-  res.send(base32secret())
-  res.setHeader('Content-Type', 'text/plain')
-  res.status(200)
-})
-
-app.use('/token/uri', (req, res) => {
-  res.send(
-    generateTokenUri({
-      issuer: 'App',
-      secret: sampleUser.secret,
-      username: sampleUser.username,
-    }),
-  )
-  res.setHeader('Content-Type', 'text/plain')
-  res.status(200)
-})
-
-app.use('/token/qr', async (req, res) => {
-  const uri = generateTokenUri({
-    issuer: 'App',
-    secret: sampleUser.secret,
-    username: sampleUser.username,
-  })
-  console.log('🚀 ~ file: server.ts ~ line 33 ~ app.use ~ uri', uri)
-  const qr = await generateQR(uri)
-  console.log('🚀 ~ file: server.ts ~ line 35 ~ app.use ~ qr', qr)
-  res.setHeader('Content-Type', 'text/html')
-  res.send(`<img src="${qr}" />`)
-  res.status(200)
-  // res.status(200)
-})
-
-app.use((req, res, next) => {
-  const { secret, token } = req.query
-
-  if (secret && token) {
-    if (secret.length !== 32 || !(secret as string).match(/^[A-Z2-7]+$/)) {
-      res.status(400)
-      res.send('Invalid secret')
-      res.end()
-      return
+const totp = otipi<typeof sampleUser>({
+  issuer: 'my-issuer',
+  getUser: (req) => {
+    const user = [sampleUser].find((x) => x.username === req.query.username)
+    if (!user) {
+      return undefined
     }
-
-    if (totp(sampleUser.secret, { digits: 6 }) !== token) {
-      res.status(400)
-      res.send('Invalid token')
-      res.end()
-      return
-    }
-
-    req.user = sampleUser
-  }
-  next()
+    return { user, secret: user.secret, username: user.username }
+  },
 })
+
+app.use('/generate', totp.createToken())
+
+app.use('/token/uri', totp.generateTokenURL())
+
+app.use('/token/qr', totp.generateTokenQR())
+
+app.use(totp.authenticate())
 
 app.use('/verify', (req, res) => {
   res.setHeader('Content-Type', 'text/plain')
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,51 @@
 import crypto from 'node:crypto'
 import { encode } from 'hi-base32'
 import QR from 'qrcode'
+import _totp from 'totp-generator'
+import { Request, Response } from 'express'
+
+export interface TotpOptions {
+  period?: number | undefined
+  /**
+   * The desired SHA variant (SHA-1, SHA-224, SHA-256, SHA-384, SHA-512,
+   * SHA3-224, SHA3-256, SHA3-384, SHA3-512, SHAKE128, or SHAKE256).
+   */
+  algorithm?:
+    | 'SHA-1'
+    | 'SHA-224'
+    | 'SHA-256'
+    | 'SHA-384'
+    | 'SHA-512'
+    | 'SHA3-224'
+    | 'SHA3-256'
+    | 'SHA3-384'
+    | 'SHA3-512'
+    | 'SHAKE128'
+    | 'SHAKE256'
+  digits?: number | undefined
+  timestamp?: number | undefined
+}
+
+export interface UserData<U> {
+  user: U
+  secret: string
+  username: string
+}
+
+type PromiseOrValue<T> = T | Promise<T>
+
+export interface TotpApiOptions<U> {
+  issuer: string
+  getUser(req: Request): PromiseOrValue<UserData<U> | undefined>
+  getToken?(req: Request): PromiseOrValue<string>
+}
+
+export interface TotpMiddlewares {
+  authenticate: () => (req: Request, res: Response, next: () => void) => Promise<void>
+  generateTokenURL: () => (req: Request, res: Response, next: () => void) => Promise<void>
+  generateTokenQR: () => (req: Request, res: Response, next: () => void) => Promise<void>
+  createToken: () => (req: Request, res: Response, next: () => void) => Promise<void>
+}
 
 export function base32secret(length = 32) {
   return encode(crypto.randomBytes(32)).slice(0, length)
@@ -31,4 +76,112 @@ export function generateQR(uri: string): Promise<string> {
       resolve(uri)
     })
   })
+}
+
+const defaultOptions: Omit<TotpOptions & TotpApiOptions<unknown>, 'issuer' | 'getUser'> = {
+  digits: 6,
+  period: 30,
+  algorithm: 'SHA-1',
+  getToken: (req) => req.query.token as string,
+}
+
+export default function totp<U>(_options: TotpOptions & TotpApiOptions<U>): TotpMiddlewares {
+  const options = {
+    ...defaultOptions,
+    ..._options,
+  } as Required<TotpOptions & TotpApiOptions<U>>
+
+  async function authenticate(req: Request, res: Response, next: () => void) {
+    const resp = await options.getUser(req)
+    if (!resp) {
+      res.status(401)
+      res.send('Unauthorized')
+      res.end()
+      return
+    }
+    const { user, secret } = resp
+    const token = await options.getToken(req)
+
+    if (token) {
+      if (_totp(secret, options) !== token) {
+        res.status(400)
+        res.send('Invalid token')
+        res.end()
+        return
+      }
+
+      req.user = user
+    }
+
+    next()
+  }
+
+  async function generateTokenURL(req: Request, res: Response) {
+    const resp = await options.getUser(req)
+    if (!resp) {
+      res.status(401)
+      res.send('Unauthorized')
+      res.end()
+      return
+    }
+    const { username, secret } = resp
+
+    if (!secret || !username) {
+      res.status(400)
+      res.send('Invalid secret or username')
+      res.end()
+      return
+    }
+
+    const uri = generateTokenUri({
+      secret,
+      issuer: options.issuer,
+      username,
+    })
+
+    res.setHeader('Content-Type', 'text/plain')
+    res.status(200)
+    res.send(uri)
+  }
+
+  async function generateTokenQR(req: Request, res: Response) {
+    const resp = await options.getUser(req)
+    if (!resp) {
+      res.status(401)
+      res.send('Unauthorized')
+      res.end()
+      return
+    }
+    const { username, secret } = resp
+
+    if (!secret || !username) {
+      res.status(400)
+      res.send('Invalid secret or username')
+      res.end()
+      return
+    }
+
+    const uri = generateTokenUri({
+      secret,
+      issuer: options.issuer,
+      username,
+    })
+
+    res.setHeader('Content-Type', 'text/plain')
+    res.status(200)
+    res.send(await generateQR(uri))
+  }
+
+  async function createToken(req: Request, res: Response) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.status(200)
+    res.send(base32secret())
+  }
+
+  return {
+    authenticate: () => authenticate,
+    generateTokenURL: () => generateTokenURL,
+    generateTokenQR: () => generateTokenQR,
+    createToken: () => createToken,
+  }
 }
